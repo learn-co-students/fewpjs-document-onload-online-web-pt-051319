f0e8c5c408973c98ec98f44c46273275e09949a6
jamievullo <jvullo@protonmail.com>
2019-11-27 11:02:54 -0500

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", function() {
     newDOM();
 });
 
+
+
 function newDOM() {
     document.getElementById("text")
     .innerHTML = "This is really cool!";
 }
+
